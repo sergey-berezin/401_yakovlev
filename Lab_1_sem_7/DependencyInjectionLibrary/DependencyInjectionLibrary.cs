59447fda5bd60cbcdd1b5ab004c5f03da301d939
Никита Яковлев <s02200529@live.cs.msu.ru>
2023-09-29 07:15:03 +0300

    new: init commit with "working" code

---
+++ Lab_1_sem_7/DependencyInjectionLibrary/DependencyInjectionLibrary.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+
+namespace MyApp
+{
+    public class IOInterface 
+    {
+        public string GetTxtFileText(string filepath) 
+        {
+            string fullText = "", line;
+            StreamReader sr = new StreamReader(filepath);
+            //Read the first line of text
+            line = sr.ReadLine();
+            //Continue to read until you reach end of file
+            while (line != null)
+            {
+                fullText += line;
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            return fullText;
+        }
+        public void Print (string data)
+        {
+            Console.WriteLine(data);
+        }
+        public string? GetInput() 
+        {
+            return Console.ReadLine();
+        }
+    }
+    public class DownloadInterface
+    {
+        public void DownloadOnnxModel(string filepath = "bert_model.onnx", string filelink = "https://storage.yandexcloud.net/dotnet4/bert-large-uncased-whole-word-masking-finetuned-squad.onnx") 
+        {
+            if (!File.Exists(filepath))
+            {
+                int try_counter = 0;
+                while (true)
+                {
+                    try
+                    {
+                        using (var client = new HttpClient())
+                        {
+                            using (var s = client.GetStreamAsync(filelink))
+                            {
+                                using (var fs = new FileStream(filepath, FileMode.OpenOrCreate))
+                                {
+                                    s.Result.CopyTo(fs);
+                                }
+                            }
+                        }
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        try_counter++;
+                        if (try_counter > 3)
+                        {
+                            try_counter = -1;
+                            break;
+                        }
+                    }
+                }
+                if (try_counter == -1)
+                {
+                    throw new Exception("Error in ONNX model download.");
+                }
+            }
+        }
+    }
+}
---
+++ Lab_1_sem_7/Lab_1_sem_7/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static Semaphore printLock = new Semaphore(1, 1);
+        static CancellationTokenSource cts = new CancellationTokenSource();
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 0)  
+            {
+                return;
+            }
+            IOInterface iOInterface = new IOInterface();
+            NeuralNetwork model = new(iOInterface.GetTxtFileText(args[0]));
+            Console.WriteLine("Starting model\n");
+            model.OnnxModelInit();
+            Console.WriteLine("Model started\n");
+            string question;
+            while ((question = Console.ReadLine()) != "")
+            {
+                var answer = ProcQuestionAsync(model, question);
+            }
+            Task.WhenAll();
+        }
+        static public async Task ProcQuestionAsync(NeuralNetwork model, string question) 
+        {
+            var questionProcRes = await Task.Run(() => model.AnswerQuestionAsync(question, cts.Token));
+            Console.Write(question);
+            Console.Write(" : "); 
+            Console.WriteLine(questionProcRes);
+        }
+    }
+}
